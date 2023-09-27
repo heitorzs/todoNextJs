5216ecdf6f970c2680bb94923b0765639c8dd27f
Heitor Zimmer <heitorzs@hotmail.com>
2023-09-27 18:01:32 -0300

    add new task

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,18 +1,28 @@
+"use client"
+
 type TodoItemProps = {
-    id: string
-    title: string
-    complete: boolean
+  id: string
+  title: string
+  complete: boolean
+  toggleTodo: (id: string, complete: boolean) => void
 }
 
-export function TodoItem({ id, title, complete }: TodoItemProps) {
-    return (
-        <>
-            <li className="flex-gap-1 items-center">
-                <input id={id} type="checkbox" className="cursor-pointer peer" />
-                <label htmlFor={id} className="peer-checked:line-through">{title}</label>
-            </li>
-
-        </>
-    )
-
+export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
+  return (
+    <li className="flex gap-1 items-center">
+      <input
+        id={id}
+        type="checkbox"
+        className="cursor-pointer peer"
+        defaultChecked={complete}
+        onChange={e => toggleTodo(id, e.target.checked)}
+      />
+      <label
+        htmlFor={id}
+        className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
+      >
+        {title}
+      </label>
+    </li>
+  )
 }
